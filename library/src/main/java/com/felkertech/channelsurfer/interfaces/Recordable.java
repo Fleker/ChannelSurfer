bd03b36f00872dd9f8b8bd82c2637221cc255b04
Nick <handnf@gmail.com>
2016-04-15 01:06:41 -0400

    Version bump

--- library/src/main/java/com/felkertech/channelsurfer/interfaces/Recordable.java
+++ library/src/main/java/com/felkertech/channelsurfer/interfaces/Recordable.java
@@ -1,9 +1,10 @@
 package com.felkertech.channelsurfer.interfaces;
 
 /**
- * An interface which will be used so the session knows whether this input
+ * An interface which will be used so the session knows whether the content of this input
  * provider can be recorded using the TV recording APIs introduced in Android N
  * for Android TV
+ *
  * Created by Nick on 3/16/2016.
  */
 public interface Recordable {
